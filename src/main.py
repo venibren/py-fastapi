2d5b2f9109ccc72537a981ba4bf3c29a8fc65abb
Brendan <bakv77@gmail.com>
2025-11-24 01:22:31 -0500

    fixed root path and added router to the fastapi application

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
-# from app.api import router as api_router
+from src.app.api import router as api_router
 from src.app.core.config import settings
 
 print(f"Starting application: {settings.app_name} v{settings.app_version}")
@@ -10,7 +10,7 @@ app = FastAPI(
     title=settings.app_name,
     description=settings.app_description,
     version=settings.app_version,
-    root_path="/api",
+    # root_path="/api",
 )
 
 # Todo: Add CORS middleware
@@ -18,4 +18,4 @@ app = FastAPI(
 # Todo: Add logging middleware
 
 # Api router configuration
-# app.include_router(api_router)
+app.include_router(api_router)
