3a0283d7d285ca54c913ab707b7b45dc97375ab1
Brendan <bakv77@gmail.com>
2025-11-27 20:06:24 -0500

    testing a get and post qr endpoint

--- src/app/api/rest/v1/qr_generator.py
+++ src/app/api/rest/v1/qr_generator.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from fastapi.responses import StreamingResponse
 
+from ....models.base_schema import BaseSchema
 from ....services.qr_service import QRService
 
 router = APIRouter(
@@ -9,7 +10,14 @@ router = APIRouter(
 )
 
 
-@router.get("", response_class=StreamingResponse)
+class QRRequestBody(BaseSchema):
+    url: str = "https://resume.venibren.dev"
+    background_color: str | None = None
+    fill_color: str | None = None
+    size: int = 10
+
+
+@router.get(path="", response_class=StreamingResponse, status_code=status.HTTP_200_OK)
 async def get_qr(
     url: str = "https://resume.venibren.dev",
     background_color: str | None = None,
@@ -24,3 +32,13 @@ async def get_qr(
     buffer = qr_service.to_buffer_stream()
 
     return StreamingResponse(buffer, media_type="image/png")
+
+
+@router.post(path="", response_class=StreamingResponse, status_code=status.HTTP_200_OK)
+async def post_qr(data: QRRequestBody) -> StreamingResponse:
+    return await get_qr(
+        url=data.url,
+        background_color=data.background_color,
+        fill_color=data.fill_color,
+        size=data.size,
+    )
