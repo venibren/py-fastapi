3c43a8d700a316b94339ea825c391865d0db7060
Brendan <bakv77@gmail.com>
2025-11-30 03:59:04 -0500

    update to user test endpoint

--- src/app/api/v1/user.py
+++ src/app/api/v1/user.py
@@ -1,16 +1,32 @@
-from fastapi import APIRouter, Response, status
+from datetime import datetime
+from fastapi import APIRouter
+from uuid import UUID
+
+from src.app.core.logger import get_logger, LoggingRoute
 
 from ...models.user import User
 
-router = APIRouter(
-    prefix="/user",
-    tags=["User"],
-)
+_logger = get_logger(__name__)
+
+router = APIRouter(prefix="/user", tags=["User"], route_class=LoggingRoute)
 
 
-@router.post(path="", status_code=status.HTTP_200_OK)
-async def post_user(user: User) -> Response:
-    print(
-        f"Registering user: {user.first_name} {user.last_name} with email: {user.email}"
+@router.get(path="")
+@router.get(path="/{id}")
+async def get_user(id: UUID) -> User:
+    _logger.debug("ID: %s", id)
+
+    user = User(
+        id=id,
+        email="brendan@venibren.dev",
+        email_verified=True,
+        username="venibren",
+        nickname="Brendan",
+        discriminator="0000",
+        created_date=datetime.today(),
+        updated_date=datetime.today(),
+        is_active=True,
     )
-    return Response(status_code=status.HTTP_200_OK)
+    _logger.debug("User: %s", user)
+
+    return user
--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+from uuid import UUID
+
 from .base_schema import BaseSchema
 
 
 class User(BaseSchema):
-    id: int
-    first_name: str | None = None
-    last_name: str | None = None
-    email: str | None = None
+    id: UUID
+    email: str
+    email_verified: bool = False
+    username: str
+    nickname: str
+    discriminator: str
+    created_date: datetime
+    updated_date: datetime
+    is_active: bool = True
