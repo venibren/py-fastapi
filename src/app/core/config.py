46b4d4da6128283b50ec2b2fe1fb82ca1f88fbf3
Brendan <bakv77@gmail.com>
2025-12-07 17:54:58 -0500

    refactor config and adding pg db details

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,9 +1,7 @@
-import os
-from enum import Enum
-from pathlib import Path
-
-from pydantic import SecretStr, computed_field
+from pydantic import Field, SecretStr, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from typing import Optional
 
 
 ###################################################################
@@ -12,9 +10,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class AppSettings(BaseSettings):
     """Application configuration settings"""
 
-    app_name: str = "Experimental FastAPI App"
-    app_description: str | None = None
-    app_version: str | None = None
+    app_name: str = Field(default="Experimental FastAPI App")
+    app_description: Optional[str] = Field(default=None)
+    app_version: Optional[str] = Field(default=None)
+
+    app_base_url: Optional[str] = Field(default=None)
+    app_port: Optional[str] = Field(default=None)
+    app_root_path: str = Field(default="/api")
 
 
 ###################################################################
@@ -23,7 +25,7 @@ class AppSettings(BaseSettings):
 class LoggerSettings(BaseSettings):
     """Logger configuration settings"""
 
-    log_level: str = "DEBUG"
+    log_level: str = Field(default="DEBUG")
 
 
 ###################################################################
@@ -32,16 +34,48 @@ class LoggerSettings(BaseSettings):
 class CORSSettings(BaseSettings):
     """CORS configuration settings"""
 
-    cors_allow_origins: list[str] = ["*"]
-    cors_allow_credentials: bool = True
-    cors_allow_methods: list[str] = ["*"]
-    cors_allow_headers: list[str] = ["*"]
+    cors_allow_origins: list[str] = Field(default=["*"])
+    cors_allow_credentials: bool = Field(default=True)
+    cors_allow_methods: list[str] = Field(default=["*"])
+    cors_allow_headers: list[str] = Field(default=["*"])
+
+
+###################################################################
+### Postgres Settings
+###################################################################
+class DatabaseSettings(BaseSettings):
+    pass
+
+
+###################################################################
+### Postgres Settings
+###################################################################
+class PostgresSettings(DatabaseSettings):
+    """PostgreSQL connection settings."""
+
+    postgres_user: str = Field(default="postgres")
+    postgres_password: SecretStr = Field(default=SecretStr("postgres"))
+    postgres_server: str = Field(default="localhost")
+    postgres_port: int = Field(default=5432)
+    postgres_db: str = Field(default="postgres")
+
+    postgres_url: Optional[str] = Field(default=None, description="Full DSN")
+
+    postgres_sync_prefix: str = Field(default="postgresql://")
+    postgres_async_prefix: str = Field(default="postgresql+asyncpg://")
+
+    @computed_field
+    @property
+    def get_uri(self) -> str:
+        credentials = f"{self.postgres_user}:{self.postgres_password}"
+        location = f"{self.postgres_server}:{self.postgres_port}/{self.postgres_db}"
+        return f"{credentials}@{location}"
 
 
 ###################################################################
 ### Overall Project Settings
 ###################################################################
-class Settings(AppSettings, LoggerSettings, CORSSettings):
+class Settings(AppSettings, LoggerSettings, PostgresSettings, CORSSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
