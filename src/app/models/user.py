957530e87ed5693dd19ca8d2433bb5512216c2a2
Brendan <bakv77@gmail.com>
2025-12-15 20:06:25 -0500

    testing user table with consideration for oauth development

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,10 +1,10 @@
 from datetime import UTC, datetime
 from enum import StrEnum
-from sqlalchemy import DateTime, Enum, String
+from sqlalchemy import DateTime, Enum, String, Text, func
 from sqlalchemy.dialects.postgresql import UUID as AlUUID
 from sqlalchemy.orm import Mapped, mapped_column
 from typing import Optional
-from uuid import UUID as PyUUID, uuid8
+from uuid import UUID as PyUUID
 
 from .base_model import BaseModel
 
@@ -17,15 +17,11 @@ class UserStatus(StrEnum):
 
 
 class User(BaseModel):
-    __tablename__ = "Users"
-
-    id: Mapped[PyUUID] = mapped_column(
+    tenant_id: Mapped[PyUUID] = mapped_column(
         AlUUID(as_uuid=True),
-        primary_key=True,
-        unique=True,
-        default_factory=uuid8,
-        nullable=False,
+        nullable=True,
     )
+
     email: Mapped[str] = mapped_column(
         String(255), unique=True, index=True, nullable=False
     )
@@ -33,8 +29,39 @@ class User(BaseModel):
     email_verified_at: Mapped[Optional[datetime]] = mapped_column(
         DateTime(timezone=True), default=None, nullable=True
     )
+
+    phone: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    phone_verified_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
+
     status = Mapped[UserStatus] = mapped_column(
         Enum(UserStatus, name="user_status", native_enum=True),
         default=UserStatus.PENDING_VERIFICATION,
         nullable=False,
     )
+
+    locked_until: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
+    last_login_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+    deleted_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
+
+    def __repr(self):
+        return f"<User(id={self.id}, email={self.email}, phone={self.phone})>"
