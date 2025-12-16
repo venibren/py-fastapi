53613beddd642ef1185e64c3e6fa407dde8c188e
Brendan <bakv77@gmail.com>
2025-12-15 20:06:01 -0500

    auto generate table name and assign id as uuid

--- src/app/models/base_model.py
+++ src/app/models/base_model.py
@@ -1,5 +1,31 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.dialects.postgresql import UUID as AlUUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from uuid import UUID as PyUUID, uuid7
+
+import re
+
+_RE_1 = re.compile(r"(.)([A-Z][a-z]+)")
+_RE_2 = re.compile(r"([a-z0-9])([A-Z])")
+
+
+def to_snake_case(name: str) -> str:
+    s1 = _RE_1.sub(r"\1_\2", name)
+    return _RE_2.sub(r"\1_\2", s1).lower()
 
 
 class BaseModel(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return to_snake_case(cls.__name__)
+
+    id: Mapped[PyUUID] = mapped_column(
+        AlUUID(as_uuid=True),
+        primary_key=True,
+        unique=True,
+        default_factory=uuid7,
+        nullable=False,
+    )
+
     pass
